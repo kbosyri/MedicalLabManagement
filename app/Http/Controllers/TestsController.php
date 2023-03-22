96ceb0c4fb03d938f84e62220378a08655a0ce2a
kinanbosyri <77687080+kinanbosyri@users.noreply.github.com>
2023-03-22 12:49:23 +0200

    Added Add Element

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Element;
+use App\Models\ElementRange;
 use Illuminate\Http\Request;
 
 class TestsController extends Controller
 {
-    //
+    public function ElementsPage()
+    {
+        return view('elements');
+    }
+
+    public function AddElement(Request $request)
+    {
+        $element = new Element();
+
+        $element->name = $request->name;
+        $element->arabic_name = $request->arabic_name;
+        $element->symbol = $request->symbol;
+        $element->is_value = $request->is_value;
+        $element->is_percentage = $request->is_percentage;
+        $element->is_exist = $request->is_exist;
+
+        $element->save();
+
+        $ranges = array();
+
+        foreach($request->ranges as $range)
+        {
+            $range['id'] = $element->id;
+            array_push($ranges,$range);
+        }
+
+        ElementRange::insert($ranges);
+
+        return response()->json(['message'=>'success']);
+    }
 }
