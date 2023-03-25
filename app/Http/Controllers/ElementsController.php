cbf765ebe9f1151802b64ef78c567f562f485e4b
kinanbosyri <77687080+kinanbosyri@users.noreply.github.com>
2023-03-25 16:20:41 +0200

    Added Add Element And Add Category

--- app/Http/Controllers/ElementsController.php
+++ app/Http/Controllers/ElementsController.php
@@ -44,7 +44,85 @@ class ElementsController extends Controller
             ValueStorage::SetCategoryElementsExistValues($element->id,$request->values);
         }
 
-        return response()->json(['message'=>'Category Element Created Successfully']);
+        return response()->json([
+            'message'=>'تم إضافة المؤشر الجزئي',
+            'element'=>new CategoryElementResource($element),
+        ]);
+    }
+
+    public function AddSubcategory(Request $request)
+    {
+        $element = new CategoryElement();
+
+        $element->name = $request->name;
+        $element->arabic_name = $request->arabic_name;
+        $element->symbol = $request->symbol;
+        if($request->belongs_to_element)
+        {
+            $element->category_id = $request->element_id;
+        }
+        else if($request->belongs_to_subcategory)
+        {
+            $element->subcategory_id = $request->element_id;
+        }
+
+        $element->is_value = $request->is_value;
+        $element->is_exist = $request->is_exist;
+        $element->is_subcategory = $request->is_subcategory;
+
+        $element->save();
+
+        return response()->json([
+            'message'=>'تم إنشاء الفئة الفرعية',
+            'subcategory'=>new SubCategoryResource($element),
+        ]);
+    }
+
+    public function AddElement(Request $request)
+    {
+        $element = new Element();
+
+        $element->name = $request->name;
+        $element->arabic_name = $request->arabic_name;
+        $element->symbol = $request->symbol;
+        $element->is_value = $request->is_value;
+        $element->is_exist = $request->is_exist;
+        $element->is_category = $request->is_category;
+
+        $element->save();
+
+        if($element->is_value)
+        {
+            ValueStorage::SetElementValueRanges($element->id,$request->values);
+        }
+        else if($element->is_exist)
+        {
+            ValueStorage::SetElementExistValues($element->id,$request->values);
+        }
+
+        return response()->json([
+            'message'=>'تم إضافة المؤشر الجزئي',
+            'element'=>new ElementResource($element),
+        ]);
+    }
+
+    public function AddCategory(Request $request)
+    {
+        $element = new Element();
+
+        $element->name = $request->name;
+        $element->arabic_name = $request->arabic_name;
+        $element->symbol = $request->symbol;
+        $element->is_value = $request->is_value;
+        $element->is_exist = $request->is_exist;
+        $element->is_category = $request->is_category;
+
+        $element->save();
+
+        return response()->json([
+            'message'=>'تم إنشاء الفئة',
+            'subcategory'=>new CategoryResource($element),
+        ]);
     }
 
     public function GetSubCategories()
