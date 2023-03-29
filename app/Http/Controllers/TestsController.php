ef38f5f140e8c86e51d310d736e8aa387b4d03b5
kinanbosyri <77687080+kinanbosyri@users.noreply.github.com>
2023-03-29 12:23:45 +0200

    Added AddTestGroup And Added Delete Functions

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Element;
 use App\Models\ElementValueRange;
 use App\Models\Test;
+use App\Models\TestsGroup;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -35,4 +36,56 @@ class TestsController extends Controller
             'message'=>"تم إضافة التحليل بنجاح",
         ]);
     }
+
+    public function AddTestGroup(Request $request)
+    {
+        $group = new TestsGroup();
+
+        $group->name = $request->name;
+
+        $group->save();
+
+        $tests = [];
+
+        foreach($request->tests as $test)
+        {
+            array_push($tests,['tests_group_id'=>$group->id,'test_id'=>$test]);
+        }
+
+        DB::table('test_group_tests')->insert($tests);
+
+        return response()->json([
+            'message'=>'تم إضافة مجموعة التحاليل بنجاح'
+        ]);
+    }
+
+    public function DeleteTest($id)
+    {
+        $test = Test::find($id);
+
+        DB::table('test_elements')->where('test_id','=',$test->id)->delete();
+
+        DB::table('test_group_tests')->where('test_id','=',$test->id)->delete();
+
+        $test->delete();
+
+        return response()->json([
+            'message'=>'تم حذف التحليل'
+        ]);
+    }
+
+    public function DeleteTestGroup($id)
+    {
+        $group = TestsGroup::find($id);
+
+        DB::table('test_group_tests')->where('tests_group_id','=',$group->id)->delete();
+
+        $group->delete();
+
+        return response()->json([
+            'message'=>'تم حذف مجموعة التحاليل'
+        ]);
+    }
+
+    
 }
