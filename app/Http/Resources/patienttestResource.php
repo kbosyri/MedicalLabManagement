c6bb7d583a6fcf021b3b11bcb8863e0f93ae37a3
SALMAKAD <salmakadour853@gmail.com>
2023-04-01 23:41:56 -0700

    Added Relations To PatientTestResource

--- app/Http/Resources/patienttestResource.php
+++ app/Http/Resources/patienttestResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Http\Resources\Tests\TestResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class patienttestResource extends JsonResource
@@ -18,6 +19,9 @@ class patienttestResource extends JsonResource
             'id'=>$this->id,
             'test_date'=>$this->test_id,
             'test_cost'=>$this->test_cost,
+            'test'=> new TestResource($this->test),
+            'staff'=>new StaffResource($this->staff),
+            'pateint'=>new PatientResource($this->patient),
             'is_finished'=>$this->is_finished,
             'is_audited'=>$this->is_audited,
         ];
