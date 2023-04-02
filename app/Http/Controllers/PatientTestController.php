3d267de8dbea451b8a03e3e6aabba487c49b0021
kinanbosyri <77687080+kinanbosyri@users.noreply.github.com>
2023-04-02 13:53:32 +0300

    Added Get Methods

--- app/Http/Controllers/PatientTestController.php
+++ app/Http/Controllers/PatientTestController.php
@@ -6,13 +6,14 @@ use App\Http\Resources\patienttestResource;
 use Illuminate\Http\Request;
 use App\Models\Patienttest;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class PatientTestController extends Controller
 {
     public function add_patient_test(Request $request)
     {
         $patienttest=new Patienttest();
-        
+
         $patienttest->test_date=$request->test_date;
         $patienttest->test_id = $request->test_id;
         $patienttest->patient_id = $request->patient_id;
@@ -22,9 +23,9 @@ class PatientTestController extends Controller
             'message'=>'تم تسجيل التحاليل المطلوبة بنجاح',
               ],500); new patienttestResource($patienttest); 
     }
-        
-    
-    
+
+
+
     public function update_patient_test(Request $request, $id)
     {
         $patienttest=Patienttest::find($id);
@@ -38,6 +39,25 @@ class PatientTestController extends Controller
               ],500); new patienttestResource( $patienttest); 
     }
 
+    public function GetStaffPatientTests()
+    {
+        $patienttests = Patienttest::where('staff_id',Auth::user()->id)->where('is_finished',false)->get();
+        
+        return patienttestResource::collection($patienttests);
+    }
 
+    public function GetUnAuditedTests()
+    {
+        $tests = Patienttest::where('is_audited',false)->where('is_finished',true)->get();
+
+        return patienttestResource::collection($tests);
+    }
+
+    public function GetPatientTests()
+    {
+        $tests = Patienttest::where('patient_id',Auth::user()->id)->where('is_audited',true);
+
+        return patienttestResource::collection($tests);
+    }
 }
 
