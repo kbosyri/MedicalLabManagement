33d66e98a58b1f428a1ec3d951472b786506da6f
kinanbosyri <77687080+kinanbosyri@users.noreply.github.com>
2023-04-02 14:08:41 +0300

    Added AddValues Function

---
+++ app/Http/Controllers/PatientTestsValueController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\patienttestResource;
+use App\Models\Patienttest;
+use App\Models\PatientTestValue;
+use Illuminate\Http\Request;
+
+class PatientTestsValueController extends Controller
+{
+    public function AddValuesToTest(Request $request,$id)
+    {
+        foreach($request->values as $value)
+        {
+            $new_value = new PatientTestValue();
+
+            $new_value->patient_test_id = $id;
+            if($value['is_category_element'])
+            {
+                $new_value->category_element_id = $value['element_id'];
+                $new_value->is_category_element = true;
+            }
+            else
+            {
+                $new_value->element_id = $value['element_id'];
+            }
+            $new_value->value = $value['value'];
+
+            $new_value->save();
+        }
+
+        $test = Patienttest::find($id);
+
+        return response()->json([
+            'message'=>'تم إدخال النتائج بنجاح',
+            'test'=>new patienttestResource($test),
+        ]);
+    }
+}
