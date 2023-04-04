f0f17beeb408573b553f57bb12bd9a1fbb55c3b0
kinanbosyri <77687080+kinanbosyri@users.noreply.github.com>
2023-04-04 18:48:24 +0300

    Added Validation And Authorization To PatientTestValues

---
+++ app/Http/Requests/PatientTestValues/AddValuesToTestRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\PatientTestValues;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Facades\Auth;
+
+class AddValuesToTestRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::user()->is_admin || Auth::user()->is_lab_staff;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'values'=>['required','array']
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'=>false,
+            'message'=>'يوجد خطأ في القيم المدخلة',
+            'errors'=>$validator->errors()
+        ],400));
+    }
+}
